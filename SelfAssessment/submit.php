fdec7fe8b316fe1f4803f22a3b961ca00add3c2d
kalp <kpanwala33@gmail.com>
2020-07-05 00:09:04 +0530

    submit.php added

--- SelfAssessment/submit.php
+++ SelfAssessment/submit.php
@@ -1,8 +1,23 @@
-<!-- $_POST["End"] gives status of Unwell or Safe.
-Save status and current date in table 
- -->
-
- 
 <?php
-echo $_POST["End"];
+session_start();
+error_reporting(0);
+include("../include/config.php");
+$id=$_SESSION['id'];
+$status=$_POST["End"];
+$ret=mysqli_query($con,"SELECT * FROM self_ass WHERE id = '$id'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+    $sql="UPDATE self_ass SET status = '$status' WHERE id = '$id'";
+}
+else
+{
+    $sql="INSERT INTO self_ass(id,status)values('$id','$status')";
+}
+if ($con->query($sql) === TRUE) {
+    header("location:../user-display.html");
+}
+else
+echo "Some Error occured";
+$con->close();       
 ?>
